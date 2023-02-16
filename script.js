29550530327c48837561e644d00a1a76e4d5fad6
2Busy Nexus <112109292+2busynexus@users.noreply.github.com>
2023-02-16 14:41:06 +0000

    animation completed, 5s and 8s. sections added

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.querySelector("a").style.display = "none"
 
+const hi = document.querySelector(".intro div:nth-child(1)")
+const iAm = document.querySelector(".intro div:nth-child(2)")
+const nav = document.querySelector("nav")
+const content = document.querySelector(".container")
 
+hi.style.animation = "fade-in-intro 3s linear 2s"
+
+setTimeout( () => {
+    hi.style.display = "none"
+    iAm.style.display = "flex"
+    nav.style.display = "flex"
+    
+}, 1000) //5
+
+setTimeout( () => {
+    content.style.display = "flex"
+}, 1000) //8
