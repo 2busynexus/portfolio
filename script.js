6166c4ca25bde84fafc079b480b5646f6b00f372
Mihai <mihaimisai@gmail.com>
2023-02-27 13:57:16 +0000

    updates after paris

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const hi = document.querySelector(".intro div:nth-child(1)")
 const iAm = document.querySelector(".intro div:nth-child(2)")
 const nav = document.querySelector("nav")
 const content = document.querySelector(".container")
-const intro = document.querySelector(".intro")
 
 hi.style.animation = "fade-in-intro 3s linear 2s"
 
@@ -23,17 +22,3 @@ setTimeout( () => {
 
 
 
-document.addEventListener("scroll", () => {
-    // Check if the nav element is now sticky
-    console.log(window.scrollY)
-
-    if (window.scrollY >= 600) {
-        nav.style.position = "fixed"
-        nav.style.left = "300px"
-      console.log("nav is now stuck");
-    } else {
-        nav.style.position = "sticky"
-        nav.style.left = "0"
-      console.log("nav is not stuck");
-    }
-  });
